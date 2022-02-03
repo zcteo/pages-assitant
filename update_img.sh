7c43751f10a7936368e01de597316c203b03628f
EricTeo <zcteo.cn@gmail.com>
2022-02-03 20:26:39 +0800

    由于国内版是16点更新，所以时间改为enddate

--- update_img.sh
+++ update_img.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-mv img.jpg "bing-imgs/$(cat startdate).jpg"
+mv img.jpg "bing-imgs/$(cat enddate).jpg"
 
 json=$(curl 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1')
 
 url="https://cn.bing.com"$(echo "$json" | python3 -c 'import sys, json; print(json.load(sys.stdin)["images"][0]["url"])')
 
-echo "$json" | python3 -c 'import sys, json; print(json.load(sys.stdin)["images"][0]["startdate"])' > startdate
+echo "$json" | python3 -c 'import sys, json; print(json.load(sys.stdin)["images"][0]["enddate"])' > enddate
 
 wget "$url" -O img.jpg
