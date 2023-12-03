30cbcc78a987d3b79b87fba1495db2c7e3bd1994
EricTeo <zcteo.cn@gmail.com>
2023-12-03 19:11:24 +0800

    新增图片描述

--- commit.sh
+++ commit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git config user.name EricTeo
 git config user.email zcteo.cn@gmail.com
+git config core.autocrlf input
 git add .
 git commit -m "github actions update at $(date +%F\ %T\ %z)"
 git push
--- update_img.sh
+++ update_img.sh
@@ -6,12 +6,19 @@ dir="bing-imgs/${enddate:0:4}/${enddate:4:2}/"
 
 mkdir -p "$dir"
 
-mv img.jpg "$dir/${enddate}.jpg"
+cp img.jpg "$dir/${enddate}.jpg"
+
+cp json.json "$dir/${enddate}.json"
 
 json=$(curl 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1')
 
 url="https://cn.bing.com"$(echo "$json" | python3 -c 'import sys, json; print(json.load(sys.stdin)["images"][0]["url"])')
 
 echo "$json" | python3 -c 'import sys, json; print(json.load(sys.stdin)["images"][0]["enddate"])' > enddate
+
+echo "$json" | python3 -c \
+    'import sys, json; img = json.load(sys.stdin)["images"][0]; \
+     ret = {"title": img["title"], "desc": img["copyright"]}; \
+     print(json.dumps(ret, ensure_ascii=False))' > json.json
 
 wget "$url" -O img.jpg
